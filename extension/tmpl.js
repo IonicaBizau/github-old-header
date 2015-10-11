98fc908255aa66ee7dd10c0b69576249492d846c
Ionică Bizău <bizauionica@gmail.com>
2015-10-11 11:30:51 +0300

    Replaced load with DOMContentLoaded

--- extension/tmpl.js
+++ extension/tmpl.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function () {
+window.addEventListener("DOMContentLoaded", function () {
     var script = document.createElement("script");
     script.innerHTML = "{lib}";
     (document.head || document.documentElement).appendChild(script);
